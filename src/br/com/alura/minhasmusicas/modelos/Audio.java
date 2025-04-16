592cea407615d5f74e014297c865a852a4aaecbc
FabioJaveiro <dev.fabioalmeida@proton.me>
2025-04-16 16:04:33 -0300

    primeiras classes

---
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -0,0 +1,46 @@
+package br.com.alura.minhasmusicas.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classificacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public void setTotalReproducoes(int totalReproducoes) {
+        this.totalReproducoes = totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public void setTotalCurtidas(int totalCurtidas) {
+        this.totalCurtidas = totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(double classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void curte() {
+        this.totalCurtidas++;
+    }
+    public void reproduz() {
+        this.totalReproducoes++;
+    }
+}
